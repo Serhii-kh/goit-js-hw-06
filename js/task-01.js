818bc84a3b04ad6da97c9c03aeb46c591c1d3645
Serhii Naboka <drifterzzz@gmail.com>
2023-02-16 23:23:21 +0200

    first done

--- js/task-01.js
+++ js/task-01.js
@@ -1,31 +1,13 @@
 const categoriesRef = document.querySelector("#categories");
 const numberOfCategories = categoriesRef.children.length;
+const subCategoriesRef = document.querySelectorAll('.item');
 
 console.log(`Number of categories: ${numberOfCategories}`);
 
-const titlesRef = document.querySelectorAll("h2");
-const elementsInCategory = document.querySelectorAll(".item ul");
-const items = document.querySelectorAll('.item');
-
-
-
-
-// // const getTitles = () =>
-//   titlesRef.forEach(el => {
-//     console.log(`Category: `, el.textContent);
-//   });
-
-// // const getNumberOfElements = () =>
-//   elementsInCategory.forEach((el) => {
-//     console.log(`Elements: `, el.children.length);
-//   });
-
-// getTitles();
-// getNumberOfElements();
-
-
-console.log(items);
-
-items.forEach(el => {
-	console.log(`Elements: `, el.children.length);
+subCategoriesRef.forEach(el => {
+	console.log(`Category: `, el.firstElementChild.textContent);
+	console.log(`Elements: `, el.lastElementChild.children.length);
+	
 });
+
+
