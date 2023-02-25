df2a8763a5765edc260db6849034a0ca35d25eb1
Serhii Naboka <drifterzzz@gmail.com>
2023-02-25 15:56:32 +0200

    9th done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body");
+const spanColorRef = document.querySelector(".color");
+const changeColorBtnRef = document.querySelector(".change-color");
+
+const getRandomBodyBgColor = () => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanColorRef.textContent = getRandomHexColor();
+};
+
+changeColorBtnRef.addEventListener("click", getRandomBodyBgColor);
