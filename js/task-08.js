7fb00cb1672e1482635561e89c81e13b3e2dfd6d
Serhii Naboka <drifterzzz@gmail.com>
2023-02-25 15:33:18 +0200

    update task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,2 +1,20 @@
-const loginFormRef = document.querySelector(".login-form");
+const formRef = document.querySelector(".login-form");
+const onFormSubmit = (event) => {
+  event.preventDefault();
 
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  const formData = {
+    email,
+    password,
+  };
+
+  if (email === "" || password === "") {
+    alert(`Please fill in all fields!`);
+  }
+
+  console.log(formData);
+  event.currentTarget.reset();
+};
+
+formRef.addEventListener("submit", onFormSubmit);
