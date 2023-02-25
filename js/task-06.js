0a06f3cf38b021057f08f5bee3cb938ff5b1602a
Serhii Naboka <drifterzzz@gmail.com>
2023-02-25 12:19:39 +0200

    update 6th task

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const inputRef = document.querySelector("#validation-input");
 const inputLength = Number(inputRef.getAttribute("data-length"));
 
 const inputCheck = (event) => {
-  if (inputLength === event.target.value.length) {
+  if (inputLength === event.currentTarget.value.length) {
     inputRef.classList.remove("invalid");
     inputRef.classList.add("valid");
   } else {
